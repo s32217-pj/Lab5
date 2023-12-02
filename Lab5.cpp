25b74644c9acfa9923a4ac24f46cc0c1b1631c2e
s32217 <s32217@GDA403-13>
2023-12-02 11:31:52 +0100

    Add project files.

---
+++ Lab5.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void zad1()
+{
+    int elem_count = 0;
+    cout << "Podaj ilosc elementow wektora: ";
+    cin >> elem_count;
+
+    vector<int> out(elem_count);
+    for (int x = 0; x < elem_count; x++)
+    {
+        cout << "Podaj liczbe [" << x + 1 << "/" << elem_count << "]\n";
+        cin >> out[x];
+    }
+
+    cout << "Element maksymalny: " << *max_element(out.begin(), out.end())<<"\n";
+    cout << "Indeks elementu maksymalnego: " << max_element(out.begin(), out.end()) - out.begin() << "\n";
+}
+
+void zad2()
+{
+    int rozmiar = 0;
+    cout << "Wpisz rozmiar tablicy: ";
+    cin >> rozmiar;
+
+    int* arr = new int[rozmiar];
+    for (int x = 0; x < rozmiar; x++)
+    {
+        cout << "Podaj liczbe [" << x + 1 << "/" << rozmiar << "]\n";
+        cin >> arr[x];
+    }
+
+    int mid = rozmiar / 2;
+
+    for (int x = 0; x < mid; x++)
+        cout<<arr[x]<<"\t"<<arr[mid + x]<<"\n";
+
+    for (int x = 0; x < mid; x++)
+        swap(arr[x], arr[mid + x]);
+
+    cout << "\n\nAfter swap: \n";
+
+
+    for (int x = 0; x < mid; x++)
+        cout << arr[x] << "\t" << arr[mid + x] << "\n";
+}
+
+int main()
+{
+    zad1();
+    zad2();
+}
